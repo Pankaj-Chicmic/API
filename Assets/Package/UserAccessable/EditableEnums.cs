2f9b5bd9ab89bc3c15d69cf23f2bed77e326410a
Pankaj Kumar <pankaj.kumar@chicmicstudios.in>
2025-05-07 12:03:13 +0530

    Add project files.

---
+++ Assets/Package/UserAccessable/EditableEnums.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace EasyAPI
+{
+    namespace RunTime
+    {
+        [Serializable]
+        public enum PayLoadEnum
+        {
+
+    ExampleRequestPayload
+}
+
+        [Serializable]
+        public enum ResponseEnum
+        {
+
+    ExampleResponsePayload
+}
+
+        [Serializable]
+        public enum EndPoints
+        {
+
+    Example,
+    Example1
+}
+    }
+}
