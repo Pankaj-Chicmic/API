291de4f068a4bd5fbaa99149b51ae22542e388c7
Pankaj Kumar <pankaj.kumar@chicmicstudios.in>
2025-05-08 14:44:02 +0530

    Fixed API Manager

--- Assets/Package/NonEditor/Request/APIManager.cs
+++ Assets/Package/NonEditor/Request/APIManager.cs
@@ -4,14 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Networking;
+
 namespace EasyAPI
 {
     namespace RunTime
     {
+        /// <summary>
+        /// Handles API communication, manages requests, retries, response parsing and validation.
+        /// </summary>
         public class APIManager : MonoBehaviourSingletonPersistent<APIManager>
         {
             [SerializeField] private Settings settings;
 
+            /// <summary>
+            /// Gets the payload and response types for the specified endpoint.
+            /// </summary>
+            /// <param name="endPoint">The endpoint to look up.</param>
+            /// <param name="payLoadEnum">The payload type associated with the endpoint.</param>
+            /// <param name="responseEnum">The response type associated with the endpoint.</param>
+            /// <returns>True if types were found successfully; otherwise false.</returns>
             public bool GetResponseTypeAndPayloadType(EndPoints endPoint, out PayLoadEnum payLoadEnum, out ResponseEnum responseEnum)
             {
                 RequestClass requestClass = settings.GetRequestClass(endPoint);
@@ -26,7 +37,20 @@ namespace EasyAPI
                 return true;
             }
 
-            public void HitAPI<T1, T2>(EndPoints endPoint, T1 payload, List<HeaderKeysAndValue> headerKeysAndValues, Action<T2> response = null, Action<float> progress = null, Dictionary<string, string> queryParams = null) where T1 : RequestPayloadBase where T2 : RequestResponseBase, new()
+            /// <summary>
+            /// Sends an API request with the given endpoint, payload, and headers, and processes the response.
+            /// </summary>
+            /// <typeparam name="T1">Type of payload.</typeparam>
+            /// <typeparam name="T2">Type of response.</typeparam>
+            /// <param name="endPoint">API endpoint to hit.</param>
+            /// <param name="payload">Request payload (can be null if not required).</param>
+            /// <param name="headerKeysAndValues">List of headers to include.</param>
+            /// <param name="response">Callback for the API response.</param>
+            /// <param name="progress">Callback for tracking download progress.</param>
+            /// <param name="queryParams">Optional query parameters.</param>
+            public void HitAPI<T1, T2>(EndPoints endPoint, T1 payload, List<HeaderKeysAndValue> headerKeysAndValues, Action<T2> response = null, Action<float> progress = null, Dictionary<string, string> queryParams = null)
+                where T1 : RequestPayloadBase
+                where T2 : RequestResponseBase, new()
             {
                 try
                 {
@@ -64,17 +88,17 @@ namespace EasyAPI
                         }
                     }
 
-
                     string jsonData = payload == null ? " " : JsonUtility.ToJson(payload);
                     int retryRemaining = requestClass.retryInfo.overrideValue ? requestClass.retryInfo.overridenValue : settings.GetAPIConfig().defaultRetryCount;
                     int requestTimeout = requestClass.requestTimeout.overrideValue ? requestClass.requestTimeout.overridenValue : settings.GetAPIConfig().defaultRequestTimeout;
                     DataType dataType = requestClass.dataTypeStruct.dataTypeOverride ? requestClass.dataTypeStruct.dataType : settings.GetAPIConfig().dataType;
-
                     string contentType = requestClass.contentTypeStruct.contentTypeOverride ? requestClass.contentTypeStruct.contentType : settings.GetAPIConfig().ContentType(dataType);
+
                     if (headerKeysAndValues == null)
                     {
                         headerKeysAndValues = new List<HeaderKeysAndValue>();
                     }
+
                     headerKeysAndValues.Add(new HeaderKeysAndValue() { key = "Content-Type", value = contentType });
 
                     KeepSendingRequest(requestClass.requestTypes, dataType, requestTimeout, retryRemaining, AppendQueryParams(requestClass.endPoint, queryParams), jsonData, headerKeysAndValues, response, progress);
@@ -85,6 +109,10 @@ namespace EasyAPI
                     return;
                 }
             }
+
+            /// <summary>
+            /// Retries the API request if necessary, until max retries are exhausted or a successful response is received.
+            /// </summary>
             private void KeepSendingRequest<T>(RequestTypes requestTypes, DataType dataType, int requestTimeout, int retryRemaining, string endPoint, string jsonData, List<HeaderKeysAndValue> headerKeysAndValues, Action<T> response = null, Action<float> progress = null) where T : RequestResponseBase, new()
             {
                 retryRemaining--;
@@ -111,6 +139,10 @@ namespace EasyAPI
                     StartCoroutine(DownloadProgress(unityWebRequest, progress));
                 }
             }
+
+            /// <summary>
+            /// Dispatches a UnityWebRequest based on request type (GET/POST/etc).
+            /// </summary>
             private UnityWebRequestAsyncOperation SendRequest<T>(RequestTypes requestTypes, DataType dataType, int requestTimeout, string endPoint, string jsonData, List<HeaderKeysAndValue> headerKeysAndValues, Action<T> response) where T : RequestResponseBase, new()
             {
                 UnityWebRequestAsyncOperation unityWebRequest = null;
@@ -118,63 +150,38 @@ namespace EasyAPI
                 {
                     case RequestTypes.GET:
                         unityWebRequest = Requests.Get(settings.GetAPIConfig().baseUrl + endPoint.ToString(), dataType, jsonData, requestTimeout, headerKeysAndValues,
-                        (responseString) =>
-                        {
-                            HandleSuccessResponse(responseString, response);
-                        },
-                        (code, responseString) =>
-                        {
-                            HandleFailureResponse(code, responseString, response);
-                        },
-                        (code) =>
-                        {
-                            HandleOtherResponse(code, response);
-                        }
-                       );
+                        (responseString) => HandleSuccessResponse(responseString, response),
+                        (code, responseString) => HandleFailureResponse(code, responseString, response),
+                        (code) => HandleOtherResponse(code, response));
                         break;
+
                     case RequestTypes.POST:
                         unityWebRequest = Requests.Post(settings.GetAPIConfig().baseUrl + endPoint.ToString(), dataType, jsonData, requestTimeout, headerKeysAndValues,
-                            (responseString) =>
-                            {
-                                HandleSuccessResponse(responseString, response);
-                            }, (code, responseString) =>
-                            {
-                                HandleFailureResponse(code, responseString, response);
-                            }, (code) =>
-                            {
-                                HandleOtherResponse(code, response);
-                            });
+                            (responseString) => HandleSuccessResponse(responseString, response),
+                            (code, responseString) => HandleFailureResponse(code, responseString, response),
+                            (code) => HandleOtherResponse(code, response));
                         break;
+
                     case RequestTypes.PUT:
                         unityWebRequest = Requests.PUT(settings.GetAPIConfig().baseUrl + endPoint.ToString(), dataType, jsonData, requestTimeout, headerKeysAndValues,
-                            (responseString) =>
-                            {
-                                HandleSuccessResponse(responseString, response);
-                            }, (code, responseString) =>
-                            {
-                                HandleFailureResponse(code, responseString, response);
-                            }, (code) =>
-                            {
-                                HandleOtherResponse(code, response);
-                            });
+                            (responseString) => HandleSuccessResponse(responseString, response),
+                            (code, responseString) => HandleFailureResponse(code, responseString, response),
+                            (code) => HandleOtherResponse(code, response));
                         break;
+
                     case RequestTypes.DELETE:
                         unityWebRequest = Requests.Delete(settings.GetAPIConfig().baseUrl + endPoint.ToString(), dataType, jsonData, requestTimeout, headerKeysAndValues,
-                            (responseString) =>
-                            {
-                                HandleSuccessResponse(responseString, response);
-                            }, (code, responseString) =>
-                            {
-                                HandleFailureResponse(code, responseString, response);
-                            }, (code) =>
-                            {
-                                HandleOtherResponse(code, response);
-                            });
+                            (responseString) => HandleSuccessResponse(responseString, response),
+                            (code, responseString) => HandleFailureResponse(code, responseString, response),
+                            (code) => HandleOtherResponse(code, response));
                         break;
                 }
                 return unityWebRequest;
             }
 
+            /// <summary>
+            /// Handles failed backend responses by attempting to parse error data and passing it to callback.
+            /// </summary>
             private void HandleFailureResponse<T>(int responseCode, string responseString, Action<T> response) where T : RequestResponseBase, new()
             {
                 T responseData = new T();
@@ -194,6 +201,9 @@ namespace EasyAPI
                 }
             }
 
+            /// <summary>
+            /// Handles successful responses and deserializes them into expected response type.
+            /// </summary>
             private void HandleSuccessResponse<T>(string responseString, Action<T> response) where T : RequestResponseBase, new()
             {
                 T responseData = new T();
@@ -211,6 +221,9 @@ namespace EasyAPI
                 }
             }
 
+            /// <summary>
+            /// Handles unknown or non-JSON parse able responses by invoking callback with error info.
+            /// </summary>
             private void HandleOtherResponse<T>(int code, Action<T> response, string message = null) where T : RequestResponseBase, new()
             {
                 T responseData = new T();
@@ -220,7 +233,9 @@ namespace EasyAPI
                 response?.Invoke(responseData);
             }
 
-
+            /// <summary>
+            /// Coroutine that continuously updates download progress via callback until complete.
+            /// </summary>
             private IEnumerator DownloadProgress(UnityWebRequestAsyncOperation operation, Action<float> progress)
             {
                 while (!operation.isDone)
@@ -230,7 +245,12 @@ namespace EasyAPI
                 }
             }
 
-
+            /// <summary>
+            /// Appends query parameters to a URL.
+            /// </summary>
+            /// <param name="url">Base URL to append to.</param>
+            /// <param name="queryParams">Key-value query parameters.</param>
+            /// <returns>URL with query string appended.</returns>
             private string AppendQueryParams(string url, Dictionary<string, string> queryParams)
             {
                 if (queryParams == null || queryParams.Count == 0) return url;
